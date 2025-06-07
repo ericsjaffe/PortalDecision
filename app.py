a01a9d6436b790ae2c1dc689e468d6d1458aa70c
ericsjaffe <77411213+ericsjaffe@users.noreply.github.com>
2025-06-07 17:51:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template
 from scraper import get_transfer_updates
 
 app = Flask(__name__)
@@ -7,10 +7,6 @@ app = Flask(__name__)
 def index():
     updates = get_transfer_updates()
     return render_template("index.html", updates=updates)
-
-@app.route("/api/updates")
-def api_updates():
-    return jsonify(get_transfer_updates())
 
 if __name__ == "__main__":
     app.run(debug=True)
